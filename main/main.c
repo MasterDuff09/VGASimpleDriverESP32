0039ff910de160126b16a21d0013129ac5ed46b4
markd <mark.denrad@gmail.com>
2025-05-12 21:53:25 +0200

    added print to test signal values

--- main/main.c
+++ main/main.c
@@ -4,6 +4,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_heap_caps.h"
+#include "res.h"
+
 
 /*
 L'idea è di fare un resizing della risoluzione VGA da 640x480 a 320x480.
@@ -16,48 +18,6 @@ Per risparmiare memoria, calcolo direttamente il numero di bit complessivi neces
 Il tutto salvato nell'array allocato nell'heap, per comandare ogni singolo pixel.
 */
 
-#define H_RES_ACT_FRAMES                320
-#define H_FRONT_PORCH_FRAMES            8
-#define H_SYNC_PULSE_FRAMES             48
-#define H_BACK_PORCH_FRAMES             24
-#define H_BLANKING_FRAMES               (H_FRONT_PORCH_FRAMES + H_SYNC_PULSE_FRAMES + H_BACK_PORCH_FRAMES)
-
-#define V_RES_ACT_FRAMES                480
-#define V_FRONT_PORCH_FRAMES            10
-#define V_SYNC_PULSE_FRAMES             2
-#define V_BACK_PORCH_FRAMES             33
-#define V_BLANKING_FRAMES               (V_FRONT_PORCH_FRAMES + V_SYNC_PULSE_FRAMES + V_BACK_PORCH_FRAMES)
-
-#define RGB_RES_BITS                    3
-#define H_SYNC_V_SYNC_BITS              2
-#define ACTIVE_BITS                     (RGB_RES_BITS + H_SYNC_V_SYNC_BITS)
-
-#define ROW_ACTIVE_BITS                 (H_RES_ACT_FRAMES * ACTIVE_BITS) // 320 frames * 5 bits (01011 --> 0R, 1G, 0B, 1H, 1V)
-#define ROW_FRONT_PORCH_BITS            (H_FRONT_PORCH_FRAMES * H_SYNC_V_SYNC_BITS) //only H/V-SYNC bits
-#define ROW_SYNC_PULSE_BITS             (H_SYNC_PULSE_FRAMES * H_SYNC_V_SYNC_BITS)
-#define ROW_BACK_PORCH_BITS             (H_BACK_PORCH_FRAMES * H_SYNC_V_SYNC_BITS)
-
-#define V_ACTIVE_ROW_TOTAL_BITS         (ROW_ACTIVE_BITS + ROW_FRONT_PORCH_BITS + ROW_SYNC_PULSE_BITS + ROW_BACK_PORCH_BITS)
-#define V_ACTIVE_ROW_TOTAL_BYTES        (V_ACTIVE_ROW_TOTAL_BITS / 8)
-
-#define V_BLANKING_ROW_TOTAL_BITS       ((H_RES_ACT_FRAMES + H_BLANKING_FRAMES) * 2) //total row frames == 400 * 2bit (h/v-sync) == 800
-#define V_BLANKING_ROW_TOTAL_BYTES      (V_BLANKING_ROW_TOTAL_BITS / 8)
-
-#define TOTAL_ACTIVE_FRAMES_BYTES       (V_ACTIVE_ROW_TOTAL_BYTES * V_RES_ACT_FRAMES)
-#define TOTAL_BLANKING_FRAMES_BYTES     (V_BLANKING_ROW_TOTAL_BYTES * V_BLANKING_FRAMES)
-
-#define TOTAL_BYTES                     (TOTAL_ACTIVE_FRAMES_BYTES + TOTAL_BLANKING_FRAMES_BYTES)
-#define TOTAL_FRAMES                    ((H_RES_ACT_FRAMES + H_BLANKING_FRAMES) * (V_RES_ACT_FRAMES + V_BLANKING_FRAMES))
-
-#define ACTIVE_CHUNK_LEN                V_RES_ACT_FRAMES
-#define BLANKING_CHUNK_LEN              V_BLANKING_FRAMES
-#define ACTIVE_SINGLE_CHUNK_SIZE        V_ACTIVE_ROW_TOTAL_BYTES
-#define BLANKING_SINGLE_CHUNK_SIZE      V_BLANKING_ROW_TOTAL_BYTES
-
-
-
-
-
 static uint8_t* pattern_chunk_active[ACTIVE_CHUNK_LEN];
 static uint8_t* pattern_chunk_blanking[BLANKING_CHUNK_LEN];
 
@@ -65,33 +25,130 @@ static uint8_t* pattern_chunk_blanking[BLANKING_CHUNK_LEN];
 
 
 void app_main(void){
-    const char *message = "Hello, world!";
-    printf("%s\n", message);
-    fflush(stdout); // Ensure the message is printed immediately
     for (int i = 0; i < ACTIVE_CHUNK_LEN; i++){
         printf("allocating chunk %d\n", i);
         pattern_chunk_active[i] = heap_caps_malloc(ACTIVE_SINGLE_CHUNK_SIZE, MALLOC_CAP_8BIT | MALLOC_CAP_DMA);
         if (pattern_chunk_active[i] == NULL) {
-            printf("Failed to allocate memory for chunk %d\n", i);
-            fflush(stdout); // Ensure the message is printed immediately
             return;
         }
-        vTaskDelay(200 / portTICK_PERIOD_MS); // Delay to simulate some processing time
     }
-    printf("Allocated %d chunks of %d bytes each\n", ACTIVE_CHUNK_LEN, ACTIVE_SINGLE_CHUNK_SIZE);
-    fflush(stdout); // Ensure the message is printed immediately
     for (int i = 0; i < BLANKING_CHUNK_LEN; i++){
         printf("allocating chunk %d\n", i);
         pattern_chunk_blanking[i] = heap_caps_malloc(BLANKING_SINGLE_CHUNK_SIZE, MALLOC_CAP_8BIT | MALLOC_CAP_DMA);
         if (pattern_chunk_blanking[i] == NULL) {
-            printf("Failed to allocate memory for chunk %d\n", i);
-            fflush(stdout); // Ensure the message is printed immediately
             return;
         }
-        vTaskDelay(200 / portTICK_PERIOD_MS); // Delay to simulate some processing time
     }
-    printf("Allocated %d chunks of %d bytes each\n", BLANKING_CHUNK_LEN, BLANKING_SINGLE_CHUNK_SIZE);
-    fflush(stdout); // Ensure the message is printed immediately
+
+    int sig_sel = 0; // starts with h-sync
+    int current_h_sync_bit = 0;
+    for (int i = 0; i < ACTIVE_CHUNK_LEN; i++){
+        current_h_sync_bit = 0;
+        sig_sel = 0;
+        for (int j = 0; j < ACTIVE_SINGLE_CHUNK_SIZE; j++){
+            pattern_chunk_active[i][j] = 0x0;
+            for (int bit_sel = 0; bit_sel < 8; bit_sel++){
+
+                if (current_h_sync_bit < H_RES_ACT_FRAMES && sig_sel > (ACTIVE_BITS -1)) {
+                    sig_sel = 0;
+                    current_h_sync_bit++;
+                } else if ((current_h_sync_bit >= H_RES_ACT_FRAMES) &&  sig_sel > 1){
+                    sig_sel = 0;
+                    current_h_sync_bit++;
+                }
+                
+                if ((current_h_sync_bit > (H_RES_ACT_FRAMES - 1) && current_h_sync_bit < (H_RES_ACT_FRAMES + H_FRONT_PORCH_FRAMES)) || (current_h_sync_bit > (H_RES_ACT_FRAMES + H_FRONT_PORCH_FRAMES + H_SYNC_PULSE_FRAMES - 1) && current_h_sync_bit < (H_RES_ACT_FRAMES + H_BLANKING_FRAMES))){
+                    pattern_chunk_active[i][j] |= (1 << bit_sel);
+                } else if (current_h_sync_bit > (H_RES_ACT_FRAMES + H_FRONT_PORCH_FRAMES - 1) && current_h_sync_bit < (H_RES_ACT_FRAMES + H_FRONT_PORCH_FRAMES + H_SYNC_PULSE_FRAMES)){
+                    if (sig_sel == 1) pattern_chunk_active[i][j] |= (1 << bit_sel);
+                    else pattern_chunk_active[i][j] |= (0 << bit_sel);    
+                } else pattern_chunk_active[i][j] |= (1 << bit_sel);
+                sig_sel++;
+            }
+        }
+    }
+    for (int i = 0; i < ACTIVE_CHUNK_LEN; i++){
+        current_h_sync_bit = 0;
+        sig_sel = 0;
+        for (int j = 0; j < ACTIVE_SINGLE_CHUNK_SIZE; j++){
+            for (int bit_sel = 0; bit_sel < 8; bit_sel++){
+                if (current_h_sync_bit < H_RES_ACT_FRAMES) {
+
+                    if (sig_sel > (ACTIVE_BITS -1)){
+                        sig_sel = 0;
+                        current_h_sync_bit++;
+                        printf("\n");
+                    }
+
+                    switch (sig_sel){
+                        case 0:
+                            vTaskDelay(10/portTICK_PERIOD_MS);
+                            printf("current row: %d, current pixel: %d, ",i, current_h_sync_bit);
+                            printf("H_sync: %d, ", (pattern_chunk_active[i][j] >> bit_sel) & 0x1);
+                            break;
+                        case 1:
+                            printf("V_sync: %d, ", (pattern_chunk_active[i][j] >> bit_sel) & 0x1);
+                            break;
+                        case 2:
+                            printf("R: %d, ", (pattern_chunk_active[i][j] >> bit_sel) & 0x1);
+                            break;
+                        case 3:
+                            printf("G: %d, ", (pattern_chunk_active[i][j] >> bit_sel) & 0x1);
+                            break;
+                        case 4:
+                            printf("B: %d\n", (pattern_chunk_active[i][j] >> bit_sel) & 0x1);
+                            fflush(stdout);
+
+                            break;
+                        default:
+                            break;
+                    }
+                } else if (current_h_sync_bit >= H_RES_ACT_FRAMES){
+                    if (sig_sel > 1){
+                        sig_sel = 0;
+                        current_h_sync_bit++;
+                        printf("\n");
+                    }
+                    switch (sig_sel){
+                        case 0:
+                            vTaskDelay(10/portTICK_PERIOD_MS);
+                            printf("current row: %d, current pixel: %d, ",i, current_h_sync_bit);
+                            printf("H_sync: %d, ", (pattern_chunk_active[i][j] >> bit_sel) & 0x1);
+                            break;
+                        case 1:
+                            printf("V_sync: %d, R: 0, G: 0; B: 0\n", (pattern_chunk_active[i][j] >> bit_sel) & 0x1);
+                            fflush(stdout);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                sig_sel++;
+            }     
+        }
+    }
+    
+
+
+
+    for (int i = 0; i < BLANKING_CHUNK_LEN; i++){
+        current_h_sync_bit = 0;
+        sig_sel = 0;
+        for(int j = 0; j < BLANKING_SINGLE_CHUNK_SIZE; j++){
+            pattern_chunk_blanking[i][j]=0x0;
+            for (int bit_sel = 0; bit_sel < 8; bit_sel++){
+                if (sig_sel > 1){
+                    sig_sel = 0;
+                    current_h_sync_bit++;
+                }
+                if ((current_h_sync_bit > (H_RES_ACT_FRAMES + H_FRONT_PORCH_FRAMES - 1) && current_h_sync_bit < (H_RES_ACT_FRAMES + H_FRONT_PORCH_FRAMES + H_SYNC_PULSE_FRAMES) && sig_sel == 0) || (i > (V_FRONT_PORCH_FRAMES - 1) && i < (V_FRONT_PORCH_FRAMES + V_SYNC_PULSE_FRAMES) && sig_sel == 1)) pattern_chunk_active[i][j] |= (0 << bit_sel);
+                else pattern_chunk_active[i][j] |= (1 << bit_sel);
+                sig_sel++;
+            }
+        }
+    }
+    printf("done\n");
+    fflush(stdout);
     return;
 
 }
