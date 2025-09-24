828ec84e59862d208d82a60d650bd7bf68699051
markd <mark.denrad@gmail.com>
2025-09-24 23:29:53 +0200

    frame_init  working :)

--- main/main.c
+++ main/main.c
@@ -1,9 +1,18 @@
 // main.c
 #include <stdio.h>
 #include <string.h>
+#include "buffer_handler.h"
+
+#include "driver/i2s_std.h"
+#include "driver/gpio.h"
+#include "esp_check.h"
+#include "driver/i2s_common.h"
+#include "sdkconfig.h"
 
 
 void app_main(void){
+    printf("start\n");
+    frame_init();
     printf("done\n");
     fflush(stdout);
     return;
--- main_temp/pattern_handler.h
+++
@@ -1,7 +0,0 @@
-
-#include "resolution.h"
-
-int VGAinit(void);
-void VGAprint(void);
-uint8_t** pattern_memory_alloc(int frames, int chunk_size);
-void pattern_fill(uint8_t** pattern_chunk_active, uint8_t** pattern_chunk_blanking);
